aff391933da859f6cb00a1f04c57ef37474d9e3f
Azmain Adel <azmainadel47@gmail.com>
2020-05-20 12:17:37 +0600

    :art: Add quotes.ts to hold static quotes

---
+++ backend-deno/quotes.ts
@@ -0,0 +1,39 @@
+interface Quote{
+    quoteText: String;
+    quoteSource: String;
+}
+
+export const quotes: Array<Quote> = [
+    {
+        "quoteText": "ذَٰلِكَ الْكِتَابُ لَا رَيْبَ ۛ فِيهِ ۛ هُدًى لِّلْمُتَّقِينَ",
+        "quoteSource": "Al-Baqara - 2:2"
+    },
+    {
+        "quoteText": "وَكَانَ اللَّـهُ سَمِيعًا بَصِيرً‌ا",
+        "quoteSource": "An-Nisa - 4:134"
+    },
+    {
+        "quoteText": "يَا أَيُّهَا الَّذِينَ آمَنُوا أَوْفُوا بِالْعُقُودِ",
+        "quoteSource": "Al-Maeda - 5:1"
+    },
+    {
+        "quoteText": "وَكُلُوا مِمَّا رَ‌زَقَكُمُ اللَّـهُ حَلَالًا طَيِّبًا",
+        "quoteSource": "Al - Maeda - 5: 88"
+    },
+    {
+        "quoteText": "وَلَوْ شَاءَ اللَّـهُ لَجَمَعَهُمْ عَلَى الْهُدَىٰ",
+        "quoteSource": "Al-Anaam - 6:35"
+    },
+    {
+        "quoteText": "كَذَٰلِكَ زَيَّنَّا لِكُلِّ أُمَّةٍ عَمَلَهُمْ",
+        "quoteSource": "Al-Anaam - 6:108"
+    },
+    {
+        "quoteText": "قَالَ إِنِّي أَعْلَمُ مَا لَا تَعْلَمُونَ",
+        "quoteSource": "Al-Baqara - 2:30"
+    },
+    {
+        "quoteText": "إِنَّ اللَّـهَ عَلَىٰ كُلِّ شَيْءٍ قَدِيرٌ",
+        "quoteSource": "Al-Baqara - 2:20"
+    },
+];
