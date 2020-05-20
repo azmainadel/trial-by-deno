5a72b53037b25d02f32915f2ac769bf56b1f5ef4
Azmain Adel <azmainadel47@gmail.com>
2020-05-20 12:39:44 +0600

    :art: Reformat code

--- backend-deno/index.ts
+++ backend-deno/index.ts
@@ -1,9 +1,14 @@
-import { Application, Router } from "https://deno.land/x/denotrain@v0.4.4/mod.ts";
-import { quotes } from "./quotes.ts";
+import {
+    Application,
+    Router
+} from "https://deno.land/x/denotrain@v0.4.4/mod.ts";
 
-const app = new Application({});
+import {
+    quotes
+} from "./quotes.ts";
 
-const router  = new Router();
+const app = new Application({}); 
+const router = new Router();
 
 app.use("/api", router);
 
@@ -11,8 +16,8 @@ app.get("/", (ctx) => {
     return "Hello World";
 });
 
-router.get("/quotes", (ctx) =>{
-    return { "quotes": quotes};
+router.get("/quotes", (ctx) => {
+    return { "quotes": quotes };
 });
 
 await app.run(); 
