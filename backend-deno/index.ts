7e0b539d9fa3751b8f69448fe8f24ea91ee7575a
Azmain Adel <azmainadel47@gmail.com>
2020-05-20 12:17:50 +0600

    :zap: Create quotes api

--- backend-deno/index.ts
+++ backend-deno/index.ts
@@ -1,11 +1,18 @@
 import { Application, Router } from "https://deno.land/x/denotrain@v0.4.4/mod.ts";
+import { quotes } from "./quotes.ts";
 
 const app = new Application({});
 
 const router  = new Router();
 
+app.use("/api", router);
+
 app.get("/", (ctx) => {
     return "Hello World";
+});
+
+router.get("/quotes", (ctx) =>{
+    return { "quotes": quotes};
 });
 
 await app.run(); 
